27a23a25f3a06048add4124a71fee32ad0a8a9cb
samarthdalela <samarthdalela@gmail.com>
2024-02-18 04:14:48 +0530

    second commit

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const port =3000
 // app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended:true}))
 app.use(express.static("public"))
-mongoose.connect("mongodb+srv://samarthdalela12345:samarthdalela@cluster0.1nzpvrf.mongodb.net/")
+mongoose.connect("mongodb+srv://samarthdalela12345:samarthdalela@cluster0.1nzpvrf.mongodb.net/test")
 var db = mongoose.connection
+var col =db.collection("users")
 // db.on("error",()=>console.log("error in connecting to database"))
 // db.once("open",()=>console.log("connected to the data base"))
 app.get("/",(req, res)=>{
@@ -31,8 +32,38 @@ app.post("/signup",(req,res)=>{
             console.log("record os entered successfully")
         }
     })
-    return res.redirect("https://shripriti.in/")
+    return res.render(__dirname+"/sigin.ejs")
 })
+app.get("/sigin.ejs",(req, res)=>{
+    res.render(__dirname+"/sigin.ejs")
+})
+app.get("/sigup.ejs",(req, res)=>{
+    res.render(__dirname+"/index.ejs")
+})
+app.post("/signin",(req,res)=>{
+    console.log(req.body)
 
+//     db.collection('users').find({name:req.body.name, password:req.body.password}).toArray().then(function(result,err){
+// //    if(result.password){res.redirect("https://shripriti.in/")}
+//        console.log(result.password)
+//     })
+var pro = {
+    projection: { name: req.body.name, password: req.body.password,email:req.body.email }
+};
 
+db.collection("users").findOne(pro.projection, function(err, result) {
+    if (err) {
+        console.log(err);
+    } else {
+        if (result) {
+            // console.log("yes");
+            res.redirect("https://shripriti.in/")
+            console.log(result)
+        } else {
+            res.render(__dirname+"/index.ejs");
+        }
+    }
+});
+
+})
 app.listen(port,()=>{console.log("you are listening to the po9rt ", port )})
--- public/java.js
+++
@@ -1,1 +0,0 @@
-alert("connection ewstavblish")
